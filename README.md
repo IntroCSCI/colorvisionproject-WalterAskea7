9bed3ec7a388b995681beb2de4496ae3105534a3
Walter Askea <waskea@mail.csuchico.edu>
2021-03-09 04:56:41 +0000

    Just started it

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 
 ## Developer
 
-!!!Replace with your name!!!
+Walter Askea
 
 ## Example
 
--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-//Author: PUT YOUR NAME HERE
+//Author: Walter Askea
 #include <iostream>
 using namespace std;
 
